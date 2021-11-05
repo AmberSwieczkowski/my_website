4f333c2037984d1d42081e0badd4e58b2c17868c
Amber M <amberm31@protonmail.com>
2021-11-05 06:52:16 -0500

    updated testimonial timeline and comment box

---
+++ dist/scripts/testimonial_comments.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+	$('button').click(function() {
+		var comment = $('.comment__box').val();
+		$('<li>').text(comment).prependTo('.comments');
+		$('button').attr('disabled', 'true');
+		$('.character__counter').text('140');
+		$('.comment__box').val('');
+	});
+	
+	$('.comment__box').keyup(function() {
+		var commentLength = $(this).val().length;
+		var charLeft =  140 - commentLength;
+		$('.character__counter').text(charLeft);
+		
+		if (commentLength == 0) {
+			$('button').attr('disabled', 'true');
+		}
+		else if (commentLength > 140) {
+			$('button').attr('disabled', 'true');
+		}
+		else {
+			$('button').removeAttr('disabled', 'true');
+		}
+	});
+	
+	$('button').attr('disabled', 'true');
+	
+});
