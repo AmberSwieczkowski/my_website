49aa7a3395dc5c53f75ed0f9a2a4d75b73b571e4
Amber M <amberm31@protonmail.com>
2021-11-06 08:15:55 -0500

    more testimonial stuff

--- dist/scripts/testimonial_comments.js
+++ dist/scripts/testimonial_comments.js
@@ -26,3 +26,10 @@ $(document).ready(function() {
 	$('button').attr('disabled', 'true');
 	
 });
+
+function post_testimonial() {
+	var new__testimonial = document.createElement("P");
+	new__testimonial.className = "testimonial__container testimonial right";
+	new__testimonial.innerHTML = "This is a paragraph.";
+	document.getElementById("testimonial__container").appendChild(new__testimonial);
+  }
