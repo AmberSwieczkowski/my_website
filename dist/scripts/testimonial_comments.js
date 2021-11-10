cb5661e53c9f117bb290756a8386fdb2f8040bc5
Amber M <amberm31@protonmail.com>
2021-11-10 08:25:18 -0600

    updated add testimonial box

--- dist/scripts/testimonial_comments.js
+++ dist/scripts/testimonial_comments.js
@@ -1,21 +1,34 @@
+/* Testimonial Container Display */
+
+$('button').on('click', function() {
+	$('#new__testimonial__container').show();
+})
+
 $(document).ready(function() {
 	$('button').click(function() {
 		var comment = $('.comment__box').val();
-		$('<div>').text(comment).prependTo('#testimonial');
+		var user_name = $('.name__box').val();
+		var empty = '';
+		$('#name').text(empty);
+		$('#testimonial').text(empty);
+		$('<div>').text(user_name).appendTo('#name');
+		$('<div>').text(comment).appendTo('#testimonial');
 		$('button').attr('disabled', 'true');
-		$('.character__counter').text('140');
+		$('.character__counter').text('400');
+		$('.name__box').val('');
 		$('.comment__box').val('');
 	});
 	
-	$('.comment__box').keyup(function() {
-		var commentLength = $(this).val().length;
-		var charLeft =  140 - commentLength;
+	$('.comment__box, .name__box').keyup(function() {
+		var commentLength = $('.comment__box').val().length;
+		var nameLength = $('.name__box').val().length;
+		var charLeft =  400 - commentLength;
 		$('.character__counter').text(charLeft);
 		
-		if (commentLength == 0) {
+		if (commentLength == 0 || nameLength == 0) {
 			$('button').attr('disabled', 'true');
 		}
-		else if (commentLength > 140) {
+		else if (commentLength > 400) {
 			$('button').attr('disabled', 'true');
 		}
 		else {
@@ -26,3 +39,9 @@ $(document).ready(function() {
 	$('button').attr('disabled', 'true');
 	
 });
+
+
+/* Rating Display */
+$('button').on('click', function() {
+	$('.new__rating').show();
+})
