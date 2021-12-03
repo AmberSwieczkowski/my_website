c80976d082bfb458c3f7b56f0c12ef804c80fb8a
Amber M <amberm31@protonmail.com>
2021-12-03 14:29:11 -0600

    updated cookie collection game

--- scripts/cookie_collection.js
+++ scripts/cookie_collection.js
@@ -1,21 +1,25 @@
 const square = document.querySelectorAll('.cookie__square');
 const cookie = document.querySelectorAll('.cookie__image');
 const timeLeft = document.querySelector('#time__left');
+const secondsLeft = document.querySelector('#cookie__countdown__timer');
 let score = document.querySelector('#cookie__collection__score');
 
 let result = 0;
 let currentTime = timeLeft.textContent;
-const numberOfSquares = 16;
+const numberOfSquares = 15;
 
 function randomSquare() {
     square.forEach(className => {
         className.classList.remove('cookie__image');
     })
-    let randomPosition = square[Math.floor(Math.random() * numberOfSquares)];
-    randomPosition.classList.add('cookie__image');
+    if (currentTime !== 0){
+        let randomPosition = square[Math.floor(Math.random() * numberOfSquares)];
+        randomPosition.classList.add('cookie__image');
+    
+        // assign the id of the randomPosition to hitPosition for us to use later
+        hitPosition = randomPosition.id
+    }
 
-    // assign the id of the randomPosition to hitPosition for us to use later
-    hitPosition = randomPosition.id
 }
 
 square.forEach(id => {
@@ -29,7 +33,8 @@ square.forEach(id => {
 
 function moveCookie() {
     let timerId = null;
-    timerId = setInterval(randomSquare, 500);
+    timerId = setInterval(randomSquare, 700);
+
 }
 
 moveCookie();
@@ -40,7 +45,7 @@ function countDown() {
 
     if(currentTime === 0) {
         clearInterval(timerId);
-        alert(`Game Over. You collected ${result} cookies!`)
+        secondsLeft.textContent = 'Game Over!';
     }
 }
 
