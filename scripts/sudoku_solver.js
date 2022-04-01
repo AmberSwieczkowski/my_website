e4a5103f732777806c61c5fe692b27e9968a2dba
Amber M <amberm31@protonmail.com>
2022-04-01 11:32:21 -0500

    Add New Game Button to Sudoku Solver. Add async sleep function to slowly show solution for each individual box.

--- scripts/sudoku_solver.js
+++ scripts/sudoku_solver.js
@@ -11,6 +11,7 @@ var grid = [
     [0, 1, 0, 6, 8, 0, 7, 2, 4],
     [8, 0, 3, 7, 2, 0, 0, 0, 9],
 ]
+let gameboard = document.getElementById('sudokuGameboard')
 
 // var grid = [
 //     [5, 3, 0, 0, 7, 0, 0, 0, 0],
@@ -27,7 +28,8 @@ var grid = [
 let newGame = () => {
     // document.getElementById('newGameButton').disabled = true
     // document.getElementById('solveButton').removeAttribute('disabled')
-    let gameboard = document.getElementById('sudokuGameboard')
+    deleteGrid()
+    console.log('New Game')
     for (let row in grid) {
         for (let column in grid) {
             let sudokuBox = document.createElement('div')
@@ -44,7 +46,6 @@ let newGame = () => {
 }
 
 let deleteGrid = () => {
-    let gameboard = document.getElementById('sudokuGameboard')
     while (gameboard.firstChild) {
         gameboard.removeChild(gameboard.firstChild)
     }
@@ -80,7 +81,7 @@ let isPossible = (row, column, number) => {
 
 let iterations = 0
 let solvedGrid;
-let solve = () => {
+let solve = async () => {
     document.getElementById('solveButton').disabled = true
     for (let row = 0; row < 9; row++) {
         for (let column = 0; column < 9; column++) {
@@ -100,7 +101,7 @@ let solve = () => {
     solvedGrid = grid
     console.log(JSON.parse(JSON.stringify(solvedGrid)))
     deleteGrid()
-    let gameboard = document.getElementById('sudokuGameboard')
+    const sleep = ms => new Promise(r => setTimeout(r, ms));
     for (let row in solvedGrid) {
         for (let column in solvedGrid) {
             let sudokuBox = document.createElement('div')
@@ -112,6 +113,7 @@ let solve = () => {
                 sudokuBox.innerHTML = ' '
             }
             gameboard.appendChild(sudokuBox)
+            await sleep(100)
         }
     }
 }
@@ -120,4 +122,5 @@ let solve = () => {
 
 let printSolution = () => {
     solve()
+    document.getElementById('newGameButton').removeAttribute('disabled')
 }
